4a0c94c90842451b8e2a16d0148f9fe656790a7b
A.B.N.S Rishik <rishikcr72401@gmail.com>
2025-06-20 08:57:38 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,9 @@
 print("Welcome to SRM Info Bot!")
-query = input("Ask me something: ").lower()
+query = input("Ask me something (Course, Club, Fees):   ").lower()
 
 if "course" in query:
     print("SRM offers B.Tech in AI, CSE, ECE, and more.")
+    print('If you like to know more information, Please visit the official website')
 elif "club" in query:
     print("SRM has coding, robotics, dance and drama clubs.")
 elif "fees" in query:
